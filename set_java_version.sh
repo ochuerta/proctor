ca83b8f6a6589bf3082629700f836c2789c73d80
Oscar C. Huerta <ochuerta@gmail.com>
2024-06-09 19:56:57 +0000

    Adding a script to set the version of Java to Java11

---
+++ set_java_version.sh
@@ -0,0 +1,10 @@
+# Set the Java version for this project
+unset JAVA_HOME
+export JAVA_HOME=/usr/lib/jvm/java-11-11.0.10_9-zulu-11.45.27
+export PATH=$JAVA_HOME/bin:$PATH
+echo $PATH
+which java
+java -version
+
+# Start a new shell session with these variables
+#exec "$SHELL"
